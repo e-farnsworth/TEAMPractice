9615c3d0795b1e747d09aad5ddfe7ea70ff8b5d8
e-farnsworth <farnsworth3e@gmail.com>
2024-05-08 13:16:08 -0600

    adding fraction to decimal function

--- practice.py
+++ practice.py
@@ -2,8 +2,21 @@
 
 # Write individual functions here
 
+def display_frac_as_decimal(top_of_frac, bot_of_frac):
+    dec = top_of_frac / bot_of_frac
+    max_digit = max(len(str(top_of_frac)),len(str(bot_of_frac)))
+    print(top_of_frac)
+    for i in range(max_digit):
+        print('-',end='')
+    print(f' = {dec:.3f}')
+    print (f'{bot_of_frac}\n')
+    
+
+
+
 # Then add call to main
 def main():
-    pass
+    display_frac_as_decimal(52,234)
+    
 
 main()
